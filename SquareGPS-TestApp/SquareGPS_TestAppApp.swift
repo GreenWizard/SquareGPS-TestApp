cb8f19d95ea5913c9d694338855ca5a6d4b76079
Sergei Denisov <s.denisov@prequelapp.com>
2026-05-06 17:34:49 +0200

    Initial Commit

---
+++ SquareGPS-TestApp/SquareGPS_TestAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  SquareGPS_TestAppApp.swift
+//  SquareGPS-TestApp
+//
+//  Created by Sergei Denisov on 06.05.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SquareGPS_TestAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
